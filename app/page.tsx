2149a4126c9c4fb53b1bb4bc35262110f6b4c405
Heritage <ireanuoluwah@gmail.com>
2025-11-14 18:52:15 +0100

    deleted instruments for optimize build

--- app/page.tsx
+++ app/page.tsx
@@ -52,20 +52,34 @@ export default function Home() {
       <section className="padding-container flex flex-col gap-5 mt-50">
         <p className="text-center">GALLERY</p>
         <h3 className={`${bellefair.className} text-5xl text-left md:text-center`}>EXPLORE OUR GALLERY</h3>
-        <div className="flex flex-col gap-1 md:gap-3">
-          <div className="flex flex-col md:flex-row gap-1 md:gap-3">
-            <Image src="/gallery-1.svg" className="w-full md:w-[50%]" alt="" height={200} width={200}/>
-            <div className="w-full flex gap-1 md:gap-3 md:w-[50%]" >
-              <Image src="/gallery-2.jpeg" className="w-[50%]" alt="" height={200} width={200}/>
-              <Image src="/gallery-3.jpeg" className="w-[50%]" alt="" height={200} width={200}/>              
+        <div className="flex flex-col gap-3 ">
+          {/* First row */}
+          <div className="flex flex-col md:flex-row gap-3">
+            <div className="w-full md:w-[50%] h-100">
+              <Image src="/gallery-1.svg" className="w-full h-full object-cover" alt="" height={200} width={200}/>
+            </div>
+            <div className="w-full flex gap-3 md:w-[50%]" >
+              <div className="w-[50%] h-100">
+                <Image src="/gallery-2.jpeg" className="w-full h-full object-cover" alt="" height={200} width={200}/>
+              </div>
+              <div className="w-[50%] h-100">
+                <Image src="/gallery-3.jpeg" className="w-full h-full object-cover" alt="" height={200} width={200}/>              
+              </div>
             </div>
           </div>        
-          <div className="flex flex-col-reverse md:flex-row w-full gap-1 md:gap-3">
-            <div className="w-full flex gap-1 md:gap-3 md:w-[50%]">
-              <Image src="/gallery-4.jpeg" className="w-[50%]" alt="" height={200} width={200}/>
-              <Image src="/gallery-3.jpeg" className="w-[50%]" alt="" height={200} width={200}/>
+          {/* Second row */}
+          <div className="flex flex-col-reverse md:flex-row w-full gap-3 ">
+            <div className="w-full flex gap-3  md:w-[50%]">
+              <div className="w-[50%] h-100">
+                <Image src="/gallery-4.svg" className="w-full h-full object-cover" alt="" height={200} width={200}/>
+              </div>
+              <div className="w-[50%] h-100">
+                <Image src="/gallery-3.jpeg" className="w-full h-full object-cover" alt="" height={200} width={200}/>
+              </div>
             </div>
-              <Image src="/gallery-5.svg" className="w-full md:w-[50%]" alt="" height={200} width={200}/>              
+            <div className="w-full md:w-[50%] h-100">
+              <Image src="/gallery-5.svg" className="w-full h-full object-cover" alt="" height={200} width={200}/>              
+            </div>
           </div>         
         </div>
 
