3e8fff9c8befc20020eefb046f21b82c66b6b4a2
Heritage <ireanuoluwah@gmail.com>
2025-11-15 16:36:15 +0100

    text issues

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,10 +6,10 @@ const bellefair = Bellefair({ subsets: ["latin"], weight: "400" });
 const page = () => {
   return (
     <div className="">
-      <div className="h-screen w-full bg-about-cover flex flex-col relative justify-end pb-40 p-5  md:p-30 gap-5 px-32">
+      <div className="h-screen w-full bg-about-cover flex flex-col relative justify-end pb-40 p-5  md:pb-30 gap-5 px-32">
         <div className="absolute inset-0 bg-black/50 z-1 h-full w-full " />
         <div className="z-1 flex flex-col gap-3">
-          <p className="text-[#b3b3b3]">ABOUT JAZ EVENTS</p>
+          <p className="text-[#b3b3b3] w-full">ABOUT JAZ EVENTS</p>
           <h3 className={`${bellefair.className} text-3xl md:text-5xl`}>
             THE BEST EVENTS CENTER IN IBADAN
           </h3>
